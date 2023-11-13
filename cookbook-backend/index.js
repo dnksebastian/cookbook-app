34842c3fc0875496b8732a1d19ac109bb7f17f77
dnksebastian <sebastian.dulniawka@gmail.com>
2023-11-13 20:03:35 +0100

    cookbook backend

--- cookbook-backend/index.js
+++ cookbook-backend/index.js
@@ -1,59 +1,122 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
 
-const express = require('express')
-const cors = require('cors')
-
-const Recipe = require('./models/recipe')
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+const express = require('express');
+const cors = require('cors');
 
 
-app.get('/', (request, response) => {
-  response.send('<h1>Server is working</h1>')
-})
+const Recipe = require('./models/recipe');
 
-app.get('/api/recipes', (request, response) => {
-  Recipe.find({})
-  .then(recipes => {
-    response.json(recipes)
-  })
-})
+const app = express();
 
-app.post('/api/recipes', (request, response) => {
-  const body = request.body
 
-  if (!body || body.title === undefined) {
-    return response.status(400).json({ error: 'Title is missing'});
-  }
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
 
-  const recipe = new Recipe({
-    title: body.title,
-    ingredients: body.ingredients,
-    method: body.method,
-    cookingTime: body.cookingTime
-  })
+app.get('/', (_request, response) => {
+    response.send('<h1>Server is working</h1>');
+});
 
-  recipe.save().then(savedRecipe => {
-    response.json(savedRecipe)
-  })
-})
+app.get('/api/recipes', (_request, response, next) => {
+    Recipe.find({})
+        .then(recipes => {
+            response.json(recipes);
+        })
+        .catch(error => next(error));
+});
 
-app.get('/api/recipes/:id', (request, response) => {
+app.post('/api/recipes', (request, response, next) => {
+    const body = request.body;
+
+    // if (!body || body.title === undefined) {
+    //   return response.status(400).json({ error: 'Title is missing'});
+    // }
+
+    const recipe = new Recipe({
+        title: body.title,
+        ingredients: body.ingredients,
+        method: body.method,
+        cookingTime: body.cookingTime
+    });
+
+    recipe.save()
+        .then(savedRecipe => {
+            response.json(savedRecipe);
+        })
+        .catch(error => next(error));
+});
+
+app.get('/api/recipes/:id', (request, response, next) => {
     const id = request.params.id;
 
     Recipe.findById(id)
-    .then(recipe => {
-      response.json(recipe)
-    })
-  })
+        .then(recipe => {
+            if (recipe) {
+                response.json(recipe);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch (error => next(error));
+});
 
-const PORT = process.env.PORT
+
+app.delete('/api/recipes/:id', (request, response, next) => {
+    const id = request.params.id;
+
+    Recipe.findByIdAndDelete(id)
+        .then(_result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
+});
+
+app.put('/api/recipes/:id', (request, response, next) => {
+    // const body = request.body;
+
+    const { title, ingredients, method, cookingTime } = request.body;
+    const id = request.params.id;
+
+    const updatedRecipe = {
+        title: title,
+        ingredients: ingredients,
+        method: method,
+        cookingTime: cookingTime
+    };
+
+    Recipe.findByIdAndUpdate(id, updatedRecipe, { new: true, runValidators: true, context: 'query' })
+        .then(receivedRec => {
+            response.json(receivedRec);
+        })
+        .catch(error => next(error));
+});
+
+
+const unknownEndpoint = (_request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+
+const errorHandler = (error, _request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted ID' });
+    } else if (error.name === 'ValidationError') {
+        console.log('here');
+        return response.status(400).json({ error: error.message });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
--- cookbook-backend/models/recipe.js
+++ cookbook-backend/models/recipe.js
@@ -1,33 +1,46 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 const mongoURL = process.env.MONGODB_URI;
 
 console.log(`connecting to ${mongoURL}`);
 
 mongoose
-  .connect(mongoURL)
-  .then((res) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("error connecting to MongoDB: ", err.message);
-  });
+    .connect(mongoURL)
+    .then((_res) => {
+        console.log('connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('error connecting to MongoDB: ', err.message);
+    });
 
 const recipeSchema = new mongoose.Schema({
-  title: String,
-  ingredients: [String],
-  method: String,
-  cookingTime: String
+    title: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    ingredients: {
+        type: [String],
+        required: true
+    },
+    method: {
+        type: String,
+        required: true
+    },
+    cookingTime: {
+        type: String,
+        required: true
+    }
 });
 
-recipeSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+recipeSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
 });
 
-module.exports = mongoose.model("Recipe", recipeSchema);
+module.exports = mongoose.model('Recipe', recipeSchema);
